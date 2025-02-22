5545aeb575e52914aa79937c875d212b8ff15037
Adrian Ibañez <hello@adrianibanez.info>
2025-02-22 23:09:17 +0100

    fallback to original

--- src/peripheral/corebluetooth/into_bool.rs
+++ src/peripheral/corebluetooth/into_bool.rs
@@ -9,7 +9,7 @@ impl IntoBool for BOOL {
         match self {
             YES => true,
             NO => false,
-            //_ => unreachable!("Unknown Objective-C BOOL value."),
+            _ => unreachable!("Unknown Objective-C BOOL value."),
         }
     }
 }
